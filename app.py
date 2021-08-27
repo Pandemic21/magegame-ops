8640ff8d0f7edfb025dae665e542704439b72d82
Pandemic <pandemicjunk@gmail.com>
2021-08-26 20:01:03 -0400

    version 0.0.1

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, jsonify, request
-import test
+from flask import url_for
 import git
 import logging
 import os
 import random
+import json
 
 ### setup logging...
 # create logger with 'spam_application'
@@ -33,26 +34,25 @@ Make everything ajax calls
 
 
 # Global variables
-app = Flask(__name__)
+OPS_CONSOLE_VERSION = "0.0.1"
 local_files = "/opt/magegame-updater/"
 index = local_files + "index.html"
-counter = 0
+static_folder_root = "./static"
 # End global variables
 
-@app.route('/api/service',  methods=["GET"])
-def mageGameService():
-    service = request.args.get('s')
-    if service == "magegame.service":
-        tmpFile = str(random.randint(0,99))
-        os.system('systemctl status magegame.service > ' + tmpFile)
-        f = open(tmpFile, "r")
-        magegamelogs = f.read()
-        os.remove(tmpFile)
-        return magegamelogs
-    elif service == "magegame-ops.service":
-        return "Client sent: magegame-ops.service"
-    else:
-        return "not a valid service: "
+# Flask config
+app = Flask(__name__,
+            static_folder=static_folder_root,
+            template_folder='./templates')
+# End Flask config
+
+
+######################
+# Navagatable Routes #
+######################
+
+
+# Route: /
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -65,23 +65,7 @@ def index():
 
         # Button: Main
         if 'Main' in d:
-            logger.info("Request to update main branch...")
-            logger.info("Stopping magegame.service...")
-            os.system('sudo systemctl stop magegame.service')
-            logger.info("Stopped.")
-            logger.info("Pulling new version...")
-            os.system('git -C /opt/magegame/ fetch --all')
-            os.system('git -C /opt/magegame/ reset --hard orgin/main')
-            os.system('git -C /opt/magegame/ pull')
-            logger.info("Pulled.")
-            logger.info("Running npm install...")
-            os.system('rm -r "/opt/magegame/node_modules"')
-            os.system('npm --prefix /opt/magegame/ i /opt/magegame/')
-            logger.info("npm modules installed.")
-            logger.info("Restarting magegame.service...")
-            os.system('sudo systemctl start magegame.service')
-            logger.info("Restarted.")
-            logger.info("Updated.")
+            updateMainBranch()
 
         # Button: Restart service
         elif 'magegame.service' in d:
@@ -124,5 +108,133 @@ def index():
     # Return them to index.html
     return render_template('index.html')
 
+
+# Route: /home #
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+
+# Route: /update
+
+@app.route('/update', methods=['GET'])
+def update():
+    return render_template('update.html')
+
+
+# Route: /console
+
+@app.route('/console', methods=['GET'])
+def console():
+    return render_template('console.html')
+
+
+# Route: /services
+
+@app.route('/services', methods=['GET'])
+def services():
+    return render_template('services.html')
+
+
+##############
+# API Routes #
+##############
+
+# Route: /api/service
+
+@app.route('/api/service',  methods=["GET"])
+def mageGameService():
+    service = request.args.get('s')
+    if service == "magegame.service":
+        tmpFile = str(random.randint(0,99))
+        os.system('systemctl status magegame.service > ' + tmpFile)
+        f = open(tmpFile, "r")
+        magegamelogs = f.read()
+        os.remove(tmpFile)
+        return magegamelogs
+    elif service == "magegame-ops.service":
+        return "Client sent: magegame-ops.service"
+    else:
+        return "not a valid service: "
+
+
+# Route: /api/version/magegame
+
+@app.route('/api/version/magegame', methods=["GET"])
+def apiGetMageGameVersion():
+    program = request.args.get('p')
+
+    #f = open("/etc/magegame/package.json", "r")
+    f = open("../magegame/package.json", "r")
+    return json.load(f)['version']
+
+
+# Route: /api/version/opsconsole
+
+@app.route('/api/version/opsconsole', methods=["GET"])
+def apiGetOpsConsoleVersion():
+    return OPS_CONSOLE_VERSION
+
+
+# Route: /api/service/<str:service>
+
+@app.route('/api/serviceCheck',  methods=["GET"])
+def apiGetAnyService():
+    service = request.args.get('s')
+    stat = -999
+
+    print(service)
+
+    # magegame.service
+    if service == "magegame.service":
+        stat = os.system('systemctl status magegame.service')
+
+    # magegame-ops.service
+    elif service == "magegame-ops.service":
+        stat = os.system('systemctl status magegame-ops.service')
+
+    # Parse what "stat" means
+    print(stat)
+    if stat == 0:
+        return '<span class="text-success">Running</span>'
+    elif stat == -999:
+        return "Service not found"
+    else:
+        return '<span class="text-danger">NOT running</span>'
+
+
+
+
+
+
+
+
+
+##########################
+# Local Helper Functions #
+##########################
+def updateMainBranch():
+    logger.info("Request to update main branch...")
+    logger.info("Stopping magegame.service...")
+    os.system('sudo systemctl stop magegame.service')
+    logger.info("Stopped.")
+    logger.info("Pulling new version...")
+    os.system('git -C /opt/magegame/ fetch --all')
+    os.system('git -C /opt/magegame/ reset --hard orgin/main')
+    os.system('git -C /opt/magegame/ pull')
+    logger.info("Pulled.")
+    logger.info("Running npm install...")
+    os.system('rm -r "/opt/magegame/node_modules"')
+    os.system('npm --prefix /opt/magegame/ i /opt/magegame/')
+    logger.info("npm modules installed.")
+    logger.info("Restarting magegame.service...")
+    os.system('sudo systemctl start magegame.service')
+    logger.info("Restarted.")
+    logger.info("Updated.")
+
+
+
 if __name__ == '__main__':
+    app._static_folder = static_folder_root
     app.run(host='0.0.0.0', port=5001)
